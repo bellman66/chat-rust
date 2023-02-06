10dc6a0c8c9dc0d0ee280ba39a658040a4288bfa
rndso15 <rndso15@naver.com>
2023-02-06 20:52:03 +0900

    feat: div Station

--- src/lib.rs
+++ src/lib.rs
@@ -2,54 +2,46 @@ extern crate core;
 
 pub mod BroadCast {
     type ClientMap = Arc<Mutex<HashMap<String, WebSocket<TcpStream>>>>;
+    type ReceiveArc = Arc<Mutex<Receiver<WebSocket<TcpStream>>>>;
 
     use std::collections::HashMap;
-    use std::fs::read;
     use std::net::{SocketAddr, TcpListener, TcpStream};
-    use std::sync::{Arc, Mutex};
-    use std::thread;
+    use std::sync::{Arc, mpsc, Mutex};
+    use std::{thread};
+    use std::fs::read;
+    use std::sync::mpsc::{Receiver, Sender};
     use tungstenite::{accept, Message, WebSocket};
 
-    pub struct ChatSocketServer {
-        listener: TcpListener,
-        clients: ClientMap,
+    pub struct ClientStation {
+        cnt: i32,
+        sockets: ClientMap,
+        receiver: ReceiveArc
     }
 
-    impl ChatSocketServer {
-        pub fn create (addr: SocketAddr) -> ChatSocketServer {
-            println!("Hello New Chat Server!!!");
-
-            ChatSocketServer {
-                listener: match TcpListener::bind(addr) {
-                    Ok(res) => res,
-                    _ => { panic!("Failed Bind TcpListener!")}
-                },
-                clients: Arc::new(Mutex::new(HashMap::new())),
+    impl ClientStation {
+        pub fn create(_receiver: Receiver<WebSocket<TcpStream>>) -> ClientStation {
+            ClientStation {
+                cnt: 0,
+                sockets: Arc::new(Mutex::new(HashMap::new())),
+                receiver: Arc::new(Mutex::new(_receiver))
             }
         }
 
-        pub fn listening(mut self) {
-            for stream in self.listener.incoming() {
-                match accept(stream.expect("Error to Stream")) {
-                    Ok(mut stream) => {
-                        println!("Client Server in");
+        pub fn read_station(&mut self) {
+            let ref arc = self.receiver.lock().unwrap();
 
-                        thread::spawn(move || {
-                            let connection = self.handle_connection(stream).expect("Failed to Handle Conn");
-                            Self::read_chat(connection)
-                        });
-                    },
-                    _ => panic!("Critical Stream Error")
-                }
+            while let Ok(mut data) = arc.recv() {
+                thread::spawn(move || {
+                    let mut guard = &self.sockets.lock().unwrap();
+                    guard.insert(String::from("test"), data);
+
+                    let mut readSocket = guard.get_mut("test").unwrap();
+                    Self::read_chat(readSocket);
+                });
             }
         }
 
-        fn handle_connection (&mut self, stream: WebSocket<TcpStream>) -> Option<WebSocket<TcpStream>> {
-            let arc = self.clients;
-            arc.lock().unwrap().insert(String::from("ok"), stream)
-        }
-
-        fn read_chat(mut stream: WebSocket<TcpStream>) {
+        fn read_chat(stream: &mut WebSocket<TcpStream>) {
             loop {
                 match stream.read_message() {
                     Ok(msg) => {
@@ -70,6 +62,39 @@ pub mod BroadCast {
                         }
                     },
                     Err(_) => {panic!("Failed to Read Msg")}
+                }
+            }
+        }
+    }
+
+    pub struct ChatSocketServer {
+        listener: TcpListener,
+        sender: Sender<WebSocket<TcpStream>>
+    }
+
+    impl ChatSocketServer {
+        pub fn create (aAddr: SocketAddr, aSender: Sender<WebSocket<TcpStream>>) -> ChatSocketServer {
+            println!("Hello New Chat Server!!!");
+
+            ChatSocketServer {
+                listener: match TcpListener::bind(aAddr) {
+                    Ok(res) => res,
+                    _ => { panic!("Failed Bind TcpListener!")}
+                },
+                sender: aSender
+            }
+        }
+
+        pub fn listening(&mut self) {
+            for stream in self.listener.incoming() {
+                match accept(stream.expect("Error to Stream")) {
+                    Ok(mut stream) => {
+                        println!("Client Server in");
+
+                        // Process : Call Mpsc & Store Stream
+                        self.sender.send(stream).unwrap();
+                    },
+                    _ => panic!("Critical Stream Error")
                 }
             }
         }
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,21 @@
-use std::net::SocketAddr;
-use chat::BroadCast::ChatSocketServer;
+use std::net::{SocketAddr, TcpStream};
+use chat::BroadCast::{ChatSocketServer, ClientStation};
 use std::str::FromStr;
+use std::sync::mpsc;
+use std::thread;
+use tungstenite::WebSocket;
 
 fn main() {
     let addr :SocketAddr = SocketAddr::from_str("127.0.0.1:9999").unwrap();
+    let (sender, receiver) = mpsc::channel();
 
-    let mut chat_serv :ChatSocketServer = ChatSocketServer::create(addr);
+    let mut chat_serv: ChatSocketServer = ChatSocketServer::create(addr, sender);
+
+    thread::spawn(move || {
+        let mut station = ClientStation::create(receiver);
+        station.read_station();
+    });
+
     chat_serv.listening();
 }
 
